862dcc9730cc8e72642d74c83c904f9d2384a24f
Light1234smek <alexfotea12@gmail.com>
2025-08-13 13:02:02 +0300

    javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
-while(true){
-    console.log("Hello World!!");
-}
+const sect= document.querySelector("section");
+const para=document.createElement("p");
+para.textContent="We hope you enjoyed the ride.";
+sect.appendChild(para);
+const text= document.createTextNode(
+    "-the premier source for web development knowledge.");
+const linkPara= document.querySelector("p");
+linkPara.appendChild(text);
+sect.appendChild(linkPara);
+linkPara.parentNode.removeChild(linkPara);
